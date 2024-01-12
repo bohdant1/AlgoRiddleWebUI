b763e6a1ffaa21ed833aa7644df5b9748e83275b
tymob0 <87370293+tymob0@users.noreply.github.com>
2024-01-13 00:58:15 +0100

    added fire init

--- AlgoRiddleWebUI/src/app/app.component.ts
+++ AlgoRiddleWebUI/src/app/app.component.ts
@@ -7,16 +7,27 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { CustomSidenavComponent } from "./components/custom-sidenav/custom-sidenav.component";
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, CustomSidenavComponent],
+  imports: [CommonModule,
+            RouterOutlet,
+            MatToolbarModule,
+            MatButtonModule,
+            MatIconModule,
+            MatSidenavModule,
+            CustomSidenavComponent,
+            AngularFireModule,
+            AngularFireAuthModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = "AlgoRiddleWebUI";
   collapsed = signal(false);
-  sidenavWidth = computed(() => this.collapsed() ? '65px' : '250px' )
+  sidenavWidth = computed(() => this.collapsed() ? '65px' : '250px')
 }
