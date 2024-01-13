0a71859c264a325e59e132515eca1df8d9855426
tymob0 <87370293+tymob0@users.noreply.github.com>
2024-01-13 22:00:54 +0100

    added login form fields with validation

--- AlgoRiddleWebUI/src/app/pages/auth/login/login.component.ts
+++ AlgoRiddleWebUI/src/app/pages/auth/login/login.component.ts
@@ -1,21 +1,55 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule, FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
+/** @title Simple form field */
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
+  standalone: true,
+  imports: [CommonModule, 
+            MatFormFieldModule, 
+            MatInputModule, 
+            MatSelectModule, 
+            FormsModule, 
+            MatCardModule, 
+            ReactiveFormsModule,
+            MatIconModule,
+            MatButtonModule],
 })
-export class LoginComponent  {
-  constructor() {
+export class LoginComponent {
+  hide = true;
 
+  loginForm = new FormGroup({
+    email: new FormControl<string>('', [Validators.required, Validators.email]),
+    password: new FormControl<string>('', [Validators.minLength(1)])
+  })
+
+  getErrorMessageEmail() {
+    if (this.loginForm.controls.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.loginForm.controls.email.hasError('email') ? 'Not a valid email' : '';
   }
-  // ngOnDestroy(): void {
-  //   this.navbarService.display();
-  // }
-  // ngOnInit(): void {
-  //   this.navbarService.hide();
-  //   console.log("Login ngOnIt "+this.navbarService.showNavBar.value);
-  // }
+
+  getErrorMessagePassword() {
+    if (this.loginForm.controls.password.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.loginForm.controls.password.hasError('password') ? 'Not a valid password' : '';
+  }
+
+
+  onSubmit() {
+    throw new Error('Method not implemented.');
+  }
 }
