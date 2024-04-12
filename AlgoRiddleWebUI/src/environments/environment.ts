d15fbcc91749cf1a497ffc6ff85cb59e193ead84
Bohdan Tymofieienko <b.tymofieienko@gmail.com>
2024-04-12 22:02:05 +0200

    added environment.ts

---
+++ AlgoRiddleWebUI/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "none",
+        authDomain: "none",
+        projectId: "none",
+        storageBucket: "none",
+        messagingSenderId: "none",
+        appId: "none"
+    },
+    registerApi: "none",
+    baseApi: "none"
+};
