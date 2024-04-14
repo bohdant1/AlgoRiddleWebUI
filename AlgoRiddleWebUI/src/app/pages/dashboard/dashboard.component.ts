bb969ff83dd806261db9b60f385eb390a6970a7f
Bohdan Tymofieienko <b.tymofieienko@gmail.com>
2024-04-14 14:40:35 +0200

    added open buttons to table

--- AlgoRiddleWebUI/src/app/pages/dashboard/dashboard.component.ts
+++ AlgoRiddleWebUI/src/app/pages/dashboard/dashboard.component.ts
@@ -2,51 +2,58 @@ import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatPaginatorModule],
+  imports: [CommonModule,
+            MatTableModule,
+            MatPaginatorModule,
+            MatButtonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements AfterViewInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  displayedColumns: string[] = ['number', 'name', 'difficulty', 'id'];
+  dataSource = new MatTableDataSource<Problem>(PROBLEM_DATA);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   ngAfterViewInit() {
+    this.paginator.pageSize = 10;
     this.dataSource.paginator = this.paginator;
   }
 }
 
-export interface PeriodicElement {
+
+export interface Problem {
+  id: string;
+  number: number;
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  difficulty: 'easy' | 'medium' | 'hard';
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+const PROBLEM_DATA: Problem[] = [
+  { id: 'prob_1', number: 1, name: 'Two Sum', difficulty: 'easy' },
+  { id: 'prob_2', number: 2, name: 'Add Two Numbers', difficulty: 'medium' },
+  { id: 'prob_3', number: 3, name: 'Longest Substring Without Repeating Characters', difficulty: 'medium' },
+  { id: 'prob_4', number: 4, name: 'Median of Two Sorted Arrays', difficulty: 'hard' },
+  { id: 'prob_5', number: 5, name: 'Longest Palindromic Substring', difficulty: 'medium' },
+  { id: 'prob_6', number: 6, name: 'ZigZag Conversion', difficulty: 'medium' },
+  { id: 'prob_7', number: 7, name: 'Reverse Integer', difficulty: 'easy' },
+  { id: 'prob_8', number: 8, name: 'String to Integer (atoi)', difficulty: 'medium' },
+  { id: 'prob_9', number: 9, name: 'Palindrome Number', difficulty: 'easy' },
+  { id: 'prob_10', number: 10, name: 'Regular Expression Matching', difficulty: 'hard' },
+  { id: 'prob_11', number: 11, name: 'Container With Most Water', difficulty: 'medium' },
+  { id: 'prob_12', number: 12, name: 'Integer to Roman', difficulty: 'medium' },
+  { id: 'prob_13', number: 13, name: 'Roman to Integer', difficulty: 'easy' },
+  { id: 'prob_14', number: 14, name: 'Longest Common Prefix', difficulty: 'easy' },
+  { id: 'prob_15', number: 15, name: '3Sum', difficulty: 'medium' },
+  { id: 'prob_16', number: 16, name: '3Sum Closest', difficulty: 'medium' },
+  { id: 'prob_17', number: 17, name: 'Letter Combinations of a Phone Number', difficulty: 'medium' },
+  { id: 'prob_18', number: 18, name: '4Sum', difficulty: 'medium' },
+  { id: 'prob_19', number: 19, name: 'Remove Nth Node From End of List', difficulty: 'medium' },
+  { id: 'prob_20', number: 20, name: 'Valid Parentheses', difficulty: 'easy' },
 ];
+
