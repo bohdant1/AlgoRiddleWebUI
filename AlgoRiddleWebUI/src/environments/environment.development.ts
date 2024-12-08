3bb424a15da815022796b7166cb1f3d0d1fef0da
tymob0 <87370293+tymob0@users.noreply.github.com>
2024-12-08 14:42:09 +0100

    updated .gitignore  and src/environments/environment.development.ts

---
+++ AlgoRiddleWebUI/src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+// FIREBASE APIKEY IS NON-SENSATIVE
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyAMZWZiuY4lH9kzzZ4PyzufaVa-TeUESOs",
+        authDomain: "algoriddle-c4a50.firebaseapp.com",
+        projectId: "algoriddle-c4a50",
+        storageBucket: "algoriddle-c4a50.appspot.com",
+        messagingSenderId: "520813551751",
+        appId: "1:520813551751:web:f090e9c1603432b85ad822"
+    },
+    baseApi: "http://localhost:8080/api/v1/"
+};
